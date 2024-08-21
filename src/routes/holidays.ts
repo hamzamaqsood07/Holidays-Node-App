f2bebe89dffc58d1314cd3bd1b41d2c2d78ae842
Hamza Maqsood <hamzamaqsood072000@gmail.com>
2024-08-21 09:05:08 +0500

    caching with redis

--- src/routes/holidays.ts
+++ src/routes/holidays.ts
@@ -4,15 +4,27 @@ const holidaysRouter = express.Router();
 import config from 'config';
 import axios from 'axios';
 const apiKey = process.env.CALENDARIFIC_API_KEY;
-const apiUrl: string = config.get('apiUrl');
+const apiUrl: string=config.get('apiUrl');
+const cacheTTL: number=config.get('cacheTTL');
+
+import { redisClient } from '../startup/redis';
 
 holidaysRouter.get('/', async (req, res) => {
   try {
-    const { country, year } = req.query;
+    const {country,year}=req.query;
+    const redisKey=`cache:holidays/${country}/${year}`;
+    const cachedData=await redisClient.get(redisKey);
+    if(cachedData) {
+      console.log('Sending response from cache');
+      return res.send(JSON.parse(cachedData));
+    }
     const response = await axios.get(
       `${apiUrl}/holidays?country=${country}&year=${year}&api_key=${apiKey}`
     );
-    res.send(response.data.response);
+    const responseData=response.data.response;
+    // Store data in cache with an expiration time
+    await redisClient.set(redisKey, JSON.stringify(responseData), { EX: cacheTTL });
+    res.send(responseData);
   } catch (e) {
     console.error(e);
     res.status(500).send();
--- src/startup/checkEnv.ts
+++ src/startup/checkEnv.ts
@@ -1,8 +1,13 @@
 export const checkEnv = () => {
   checkEnvironmentVariable('CALENDARIFIC_API_KEY');
+  checkEnvironmentVariable('REDIS_HOST');
+  checkEnvironmentVariable('REDIS_PORT');
+  checkEnvironmentVariable('REDIS_USERNAME');
+  checkEnvironmentVariable('REDIS_PASSWORD');
+  checkEnvironmentVariable('REDIS_DB_NUMBER');
 };
 const checkEnvironmentVariable = function (variable: string) {
-  if (!process.env[variable]) {
+  if (process.env[variable]===undefined) {
     console.log(`FATAL ERROR: ${variable} is not defined.`);
     process.exit(1);
   }
